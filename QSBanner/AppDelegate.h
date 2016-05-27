eacad347d61f69e4d893c56eeb49277c8544dfd1
jingshuihuang <jingshuihuang@jingshuihuangdeimac.local>
2016-05-27 10:34:20 +0800

    Initial Commit

---
+++ QSBanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QSBanner
+//
+//  Created by jingshuihuang on 16/5/27.
+//  Copyright © 2016年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QSBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QSBanner
+//
+//  Created by jingshuihuang on 16/5/27.
+//  Copyright © 2016年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
